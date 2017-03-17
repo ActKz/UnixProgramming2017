1fda80d6c1f2f4fc17406315e310589ce5400f4b
actkz <actkz.cs02@g2.nctu.edu.tw>
2017-03-16 23:08:10 -0700

    Spec requirement complete without debugging

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <map>
 #include <fstream>
 #include <regex>
+#include <arpa/inet.h>
+#include <endian.h>
+
 using namespace std;
 
 #define TCP "/proc/net/tcp"
@@ -20,6 +23,8 @@ class sock_info{
     public:
         string local_addr;
         string rem_addr;
+        int local_port;
+        int rem_port;
         int pid;
         string cmd;
 };
@@ -81,18 +86,67 @@ void traverse_proc(Sock &tcp, Sock &udp, Sock &tcp6, Sock &udp6){
     closedir(dir_proc);
 
 }
+string hexIP_intIP(string hexIP){
+    struct in6_addr tmp_ip6;
+    struct in_addr tmp_ip;
+    char ip_str[128];
+    unsigned int ip;
+    char tmp[50];
+    if(sscanf(hexIP.data(),
+            "%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx",
+            &tmp_ip6.s6_addr[3], &tmp_ip6.s6_addr[2], &tmp_ip6.s6_addr[1], &tmp_ip6.s6_addr[0],
+            &tmp_ip6.s6_addr[7], &tmp_ip6.s6_addr[6], &tmp_ip6.s6_addr[5], &tmp_ip6.s6_addr[4],
+            &tmp_ip6.s6_addr[11], &tmp_ip6.s6_addr[10], &tmp_ip6.s6_addr[9], &tmp_ip6.s6_addr[8],
+            &tmp_ip6.s6_addr[15], &tmp_ip6.s6_addr[14], &tmp_ip6.s6_addr[13], &tmp_ip6.s6_addr[12]) == 16){
+        inet_ntop(AF_INET6, &tmp_ip6, ip_str, sizeof ip_str);
+        return string(ip_str);
+    }else{
+// old method
+//  unsigned int a, b, c, d, e, f, g, h;
+//      sscanf(hexIP.data(), "%2x%2x%2x%2x", &a, &b, &c, &d);
+//      snprintf(tmp, 50, "%u.%u.%u.%u", d, c, b, a);
+        ip = (int)strtol(hexIP.data(), NULL, 16 );
+        tmp_ip.s_addr = htonl(htobe32(ip));
+        return string(inet_ntoa(tmp_ip));
+    }
+}
 void build_conninfo(Sock &connInfo, ifstream &file){
-    string s, local_addr, rem_addr, socket;
+    string s, local_addr, rem_addr, socket, local_port, rem_port;
     file.ignore(512,'\n');
     while(file >> s){
-        file >> local_addr >> rem_addr;
+        getline(file, local_addr, ':');
+        getline(file, local_port, ' ');
+        getline(file, rem_addr, ':');
+        getline(file, rem_port, ' ');
+//      file >> local_addr >> rem_addr;
         for(int i = 0; i<6; i++)
             file >> s;
         file >> socket;
         file.ignore(512,'\n');
-        connInfo[socket].local_addr = local_addr;
-        connInfo[socket].rem_addr = rem_addr;
+        connInfo[socket].local_addr = hexIP_intIP(local_addr);
+        connInfo[socket].rem_addr = hexIP_intIP(rem_addr);
+        connInfo[socket].local_port = stoul(local_port, nullptr, 16);
+        connInfo[socket].rem_port = stoul(rem_port, nullptr, 16);
     }
+}
+void print_conns(Sock &conn, Sock &conn6, string type){
+    cout << "List of "<<type<<" connections:" << endl;
+    cout << "Proto Local Address           Foreign Address         PID/Program name and arguments" << endl;
+    for(auto it = conn.begin(); it != conn.end(); it++){
+        string l_addr = it->second.local_addr + ":" + to_string(it->second.local_port),
+               r_addr = it->second.rem_addr + ":" + to_string(it->second.rem_port),
+               pid_cmd = to_string(it->second.pid) + "/" + it->second.cmd;
+        printf("%-6s%-24s%-24s%s\n", type.data(), l_addr.data(), r_addr.data(), pid_cmd.data());
+    }
+    type += "6";
+    for(auto it = conn6.begin(); it != conn6.end(); it++){
+        string l_addr = it->second.local_addr + ":" + to_string(it->second.local_port),
+               r_addr = it->second.rem_addr + ":" + to_string(it->second.rem_port),
+               pid_cmd = to_string(it->second.pid) + "/" + it->second.cmd;
+        printf("%-6s%-24s%-24s%s\n", type.data(), l_addr.data(), r_addr.data(), pid_cmd.data());
+    }
+    cout<<endl;
+
 }
 void netstat_nap(bool tcp, bool udp, char *filt_str){
     Sock tcp_conn, udp_conn, tcp6_conn, udp6_conn;
@@ -120,10 +174,12 @@ void netstat_nap(bool tcp, bool udp, char *filt_str){
         build_conninfo(udp6_conn, net_udp6);
     }
     traverse_proc(tcp_conn, udp_conn, tcp6_conn, udp6_conn);
-    for(auto it = tcp_conn.begin(); it != tcp_conn.end(); it++){
-        cout<<"tcp_conn["<<it->first<<"]= "<<it->second.local_addr<<", "<<it->second.rem_addr<<", "<<it->second.pid<<", "<<it->second.cmd<<endl;
+    if(tcp | both){
+        print_conns(tcp_conn, tcp6_conn, "tcp");
     }
-
+    if(udp | both){
+        print_conns(udp_conn, udp6_conn, "udp");
+    }
 }
 int main(int argc, char *argv[])
 {
